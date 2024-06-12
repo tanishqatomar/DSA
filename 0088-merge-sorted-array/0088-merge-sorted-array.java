7b54eca7bf5c2a402d83dc54d9d1a59e461d2428
Tanishqa Tomar <102842674+tanishqatomar@users.noreply.github.com>
2024-06-12 16:52:06 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (19.78%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,13 +1,30 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m-1;
-        int j=n-1;
-        int k=m+n-1;
-        while(j>=0){
-            if(i>=0 && nums1[i]>nums2[j])
-                nums1[k--]=nums1[i--];
-            else
-                nums1[k--]=nums2[j--];
-        }        
+        //three pointer approach
+        int reader1 = m-1;
+        int reader2 = n-1;
+        int writer = (m+n)-1;
+        while(writer >= 0){ //till the writer does not reache 0th index
+            //checking if reader1 and reader2 have not ended
+            if(reader1 >= 0 && reader2 >= 0){   
+                if(nums1[reader1] > nums2[reader2]){
+                    nums1[writer] = nums1[reader1];
+                    reader1--;
+                }else{
+                    nums1[writer] = nums2[reader2];
+                    reader2--;
+                }
+            }
+            //either array has not exhausted
+            else if(reader1 >= 0){
+                nums1[writer] = nums1[reader1];
+                reader1--;
+            }else if(reader2 >= 0){
+                nums1[writer] = nums2[reader2];
+                reader2--;
+                
+            }
+            writer--;
+        }
     }
 }
